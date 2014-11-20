9948e1158597fc96e8eb0295285159162a9b1806
Mabry Cervin <mpcervin@uncg.edu>
2014-11-19 22:05:25 -0500

    Added more to Num instance

--- UDTup.hs
+++ UDTup.hs
@@ -7,5 +7,10 @@ data UDTup a = Up Vector a
 
 instance (Num a) => Either Num (UDTup a) where
         (+) (Up a) (Down a) = Left "Cannot add an up and down tuple"
+        (+) (Down a) (Up a) = Left "Cannot add an up and down tuple"
         (+) (Up a) (Up b) = Right $ Up $ V.zipWith (+) a b
         (+) (Down a) (Down b) = Right $ Down $ V.zipWith (+) a b
+        (-) (Up a) (Down a) = Left "Cannot subtract an up and down tuple"
+        (-) (Down a) (Up a) = Left "Cannot subtract an up and down tuple"
+        (-) (Up a) (Up b) = Right $ Up $ V.zipWith (-) a b
+        (-) (Down a) (Down b) = Right $ Down $ V.zipWith (-) a b
