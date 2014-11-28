415fc99bbaad7760d32eb5cc02cf9a82e7f8bab3
Mabry Cervin <mpcervin@uncg.edu>
2014-11-28 00:20:09 -0500

    Swapped vectors to a more tuple-like representation

--- UDTup.hs
+++ UDTup.hs
@@ -1,16 +1,12 @@
-import SymCalc
-import qualified Data.Vector as V
+module UDTup 
+    ( UDTup(..)
+    ) where
+
 import Data.Either
 
-data UDTup a = Up Vector a
-             | Down Vector a
+data UDTup a = Val a
+             | Up (UDTup a)
+             | Down (UDTup a)
+             | Up2 (UDTup a) (UDTup a)
+             | Down2 (UDTup a) (UDTup a)
 
-instance (Num a) => Either Num (UDTup a) where
-        (+) (Up a) (Down a) = Left "Cannot add an up and down tuple"
-        (+) (Down a) (Up a) = Left "Cannot add an up and down tuple"
-        (+) (Up a) (Up b) = Right $ Up $ V.zipWith (+) a b
-        (+) (Down a) (Down b) = Right $ Down $ V.zipWith (+) a b
-        (-) (Up a) (Down a) = Left "Cannot subtract an up and down tuple"
-        (-) (Down a) (Up a) = Left "Cannot subtract an up and down tuple"
-        (-) (Up a) (Up b) = Right $ Up $ V.zipWith (-) a b
-        (-) (Down a) (Down b) = Right $ Down $ V.zipWith (-) a b
